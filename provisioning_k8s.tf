cb67b090b29644f306407a2585ece06ee2daa972
Paolo Colia <paolocolix@gmail.com>
2024-05-21 20:02:05 +0200

    Fixed  terraform and vagrant with new path

--- provisioning_k8s.tf
+++ provisioning_k8s.tf
@@ -1,11 +1,11 @@
 provider "kubernetes" {
   config_context_cluster = "kubernetes"
-  config_path = "./provisioning/join/admin.conf"
+  config_path = "./VM_provisioning/_cluster_k8s_info/admin.conf"
 }
 
 provider "helm" {
   kubernetes {
-  config_path = "./provisioning/join/admin.conf"
+  config_path = "./VM_provisioning/_cluster_k8s_info/admin.conf"
   }
 }
 
