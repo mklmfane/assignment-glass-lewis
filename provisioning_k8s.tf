c6a4dc7288d879bbc3fbe5cc1bcdd9470a0ec6dc
Paolo Colia <paolocolix@gmail.com>
2024-05-21 19:29:17 +0200

    Created Terraform file

---
+++ provisioning_k8s.tf
@@ -0,0 +1,56 @@
+provider "kubernetes" {
+  config_context_cluster = "kubernetes"
+  config_path = "./provisioning/join/admin.conf"
+}
+
+provider "helm" {
+  kubernetes {
+  config_path = "./provisioning/join/admin.conf"
+  }
+}
+
+resource "helm_release" "calico" {
+  name       = "calico"
+  namespace   = "kube-system"
+  repository  = "https://docs.tigera.io/calico/charts"
+  chart       = "tigera-operator"
+  version    = "v3.28"
+}
+
+resource "kubernetes_namespace" "kiratech-test" {
+  metadata {
+    name      = "kiratech-test"
+  }
+}
+
+resource "kubernetes_job" "kube-bench" {
+  metadata {
+    name = "kube-bench"
+  }
+  spec {
+    template {
+      metadata {
+        labels = {
+          app = "kube-bench"
+        }
+      }
+      spec {
+        container {
+          name = "kube-bench"
+          image = "aquasec/kube-bench:latest"
+          command = ["kube-bench", "run", "--json", "--outputfile", "/tmp/kube-bench-results.json"]
+          volume_mount {
+            name = "kube-bench-results"
+            mount_path = "/tmp"
+          }
+        }
+        volume {
+          name = "kube-bench-results"
+          host_path {
+            path = "/tmp/kube-bench-results"
+          }
+        }
+      }
+    }
+  }
+}
