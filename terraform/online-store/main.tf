26ab575fc9d480e66ac867da45b3043dbd655b23
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-05-10 21:11:29 +0300

    Add online stoer template applied

--- terraform/online-store/main.tf
+++ terraform/online-store/main.tf
@@ -1,5 +1,15 @@
+variable "dockerconfigjson_b64" {}
+
+resource "local_file" "online_store_yaml" {
+  filename = "${path.module}/manifests/online-store.yaml"
+  content  = templatefile("${path.module}/manifests/online-store.yaml", {
+    dockerconfigjson_b64 = var.dockerconfigjson_b64
+  })
+}
+
 resource "null_resource" "apply_online_store" {
   provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/manifests/online-store-template.yaml"
+    command = "kubectl apply -f ${local_file.online_store_yaml.filename}"
   }
+  depends_on = [local_file.online_store_yaml]
 }
